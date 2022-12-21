1eb1d42a9ce48483a2defdf140cc17d3c97f680a
Joanderson Silva <jhonny_040996@hotmail.com>
2022-12-21 14:45:57 -0300

    feat:lipstick:: adicionando fonte Poppins

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,11 +7,11 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
-          href="https://fonts.gstatic.com"
+          href="https://fonts.gstatic.com" 
           crossOrigin={anonymous}
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
