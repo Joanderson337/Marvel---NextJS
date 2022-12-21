e188d08b86e22946b58ac45ca0763dccb6195d62
Joanderson Silva <jhonny_040996@hotmail.com>
2022-12-21 20:34:32 -0300

    feat:sparkles:: configurações de rotas

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com" 
-          crossOrigin={anonymous}
+          crossOrigin="anonymous"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
