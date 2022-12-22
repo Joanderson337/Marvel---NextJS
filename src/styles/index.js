f4d3f7e18469375e6e092c90ef6c7ff1b8ccec74
Joanderson Silva <jhonny_040996@hotmail.com>
2022-12-21 21:04:03 -0300

    feat :sparkles:: adicionando stitches

---
+++ src/styles/index.js
@@ -0,0 +1,20 @@
+import {createStitches} from '@stitches/react'
+
+export const {styled, globalCss, keyframes, getCssText, theme} = 
+createStitches({
+  theme: {
+    colors: {
+      red_900: "#FF0000",
+      red_800: "#FF040D",
+      while: "#FFFFFF",
+      grey_900: "#33303E",
+      grey_800: "#4E4B59",
+      grey_700: "#5F5C6B",
+      grey_600: "#7A7786",
+      grey_500: "#8C8A97",
+      grey_400: "#ACABB7",
+      grey_300: "#C6C5CE",
+      grey_200: "#E1E0E7"
+    }
+  }
+})
