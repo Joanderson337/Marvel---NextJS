372512a7eceeb51f7072255a9f9e02d86a7527c7
Joanderson Silva <jhonny_040996@hotmail.com>
2022-12-23 17:59:26 -0300

    refactor:recycle:: definindo componentes que vai renderizar apenas uma vez

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,12 @@
-import { Header } from "../components/header";
 import { SectionHero } from "../components/home/sectionHero";
 import { SectionCharacters } from "../components/home/sectionCharacters";
-import { Footer } from "../components/footer";
 
 
 export default function Home() {
   return (
     <>
-      <Header />
       <SectionHero />
       <SectionCharacters/>
-      <Footer />
     </>
   )
 }
