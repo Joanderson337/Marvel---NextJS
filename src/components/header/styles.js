8abaf155b0b21541b1892875c35f18a1ccdfbd51
Joanderson Silva <jhonny_040996@hotmail.com>
2022-12-23 14:32:47 -0300

    feat:lipstick:: style no componente header

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,9 +1,24 @@
-import {styled} from '../../styles'
+import { styled } from "../../styles";
+
+import { Container } from "../../styles/global";
 
 export const HeaderStyle = styled("header", {
-  width: '100%',
-  marginTop: 28,
-  paddingBottom: 28,
-  borderBottom: "1px solid rgba(255,255,255, 0.2)",
+  position: "absolute",
+  top: 0,
+  width: "100%",
+  height: 96,
+  borderBottom: "1px solid rgba(255, 255, 255, 0.2)",
+  display: "flex",
+  alignItems: "center",
   zIndex: 2023,
-})
+  [`${Container}`]: {
+    height: "100%",
+    borderLeft: "1px solid rgba(255, 255, 255, 0.2)",
+    display: "flex",
+    alignItems: "center",
+  },
+  ".logo": {
+    position: "relative",
+    paddingLeft: 23,
+  }
+});
