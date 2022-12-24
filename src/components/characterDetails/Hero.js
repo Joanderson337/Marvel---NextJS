329b4a883087ca4e4298ef642c8bdcda721bcc34
Joanderson Silva <jhonny_040996@hotmail.com>
2022-12-24 00:34:08 -0300

    feat:sparkles:: adicionando componente hero

---
+++ src/components/characterDetails/Hero.js
@@ -0,0 +1,30 @@
+import Image from "next/image"
+
+import { Container } from "../../styles/global"
+import { HeroStyle, ImageCharacter, InfoCharacter } from "./styles"
+
+export function CharacterHero({ dataCharacter }) {
+  return (
+    dataCharacter && (
+      <HeroStyle>
+        <Container>
+          <ImageCharacter>
+            <Image
+              src={dataCharacter.data.imagem_personagem.url}
+              width={486}
+              height={631}
+              alt="Imagem do personagem"
+            />
+          </ImageCharacter>
+          <InfoCharacter>
+            <span>A História</span>
+            <div>
+              <h1>{dataCharacter.data.nome_personagem}</h1>
+              <p>{dataCharacter.data.descricao_personagem[0].text}</p>
+            </div>
+          </InfoCharacter>
+        </Container>
+      </HeroStyle>
+    )
+  );
+}
---
+++ src/components/characterDetails/styles.js
@@ -0,0 +1,74 @@
+import { styled } from "../../styles";
+
+import { Container } from "../../styles/global";
+
+import BgDetail from "../../assets/bg-details.jpg";
+
+export const HeroStyle = styled("section", {
+  paddingTop: 96,
+  background: `url(${BgDetail.src}) no-repeat top center`,
+  borderBottom: "1px solid rgba(255, 255, 255, 0.2)",
+  backgroundAttachment: "fixed",
+  [`${Container}`]: {
+    display: "flex",
+    ai: "flex-start",
+    borderLeft: "1px solid rgba(255, 255, 255, 0.2)",
+    borderRight: "1px solid rgba(255, 255, 255, 0.2)",
+  },
+});
+
+export const ImageCharacter = styled("div", {
+  flex: 1,
+  width: "100%",
+  maxWidth: 486,
+  height: 631,
+  borderRadius: 5,
+  overflow: "hidden",
+});
+
+export const InfoCharacter = styled("div", {
+  flex: 1,
+  paddingLeft: 32,
+  display: "flex",
+  ai: "flex-start",
+  paddingTop: 154,
+  span: {
+    display: "block",
+    marginTop: 5,
+    fontWeight: 400,
+    fontSize: "1.8rem",
+    lineHeight: "2.7rem",
+    color: "#ACABB7",
+    opacity: 0.6,
+    marginRight: 19,
+  },
+  "& > div": {
+    position: "relative",
+    flex: 1,
+    maxWidth: 520,
+    paddingLeft: 22,
+    "&:before": {
+      content: "",
+      position: "absolute",
+      top: 12,
+      left: 0,
+      width: 4,
+      height: 46,
+      backgroundColor: "$red_800",
+    },
+    h1: {
+      fontWeight: 600,
+      fontSize: "6.1rem",
+      lineHeight: "7.1rem",
+      color: "$white",
+      marginBottom: 13,
+    },
+    p: {
+      fontWeight: 400,
+      fontSize: "1.6rem",
+      lineHeight: "2.4rem",
+      color: "$gray_400",
+      maxWidth: 385,
+    },
+  },
+});
